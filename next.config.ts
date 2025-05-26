8ea71e539f0a97dd75fedc203197f98d07bb4849
Yoshio Nomura <nomurin.alex103@gmail.com>
2025-05-26 22:10:52 +0900

    63rd commit

--- next.config.ts
+++ next.config.ts
@@ -11,23 +11,7 @@ const nextConfig: NextConfig = {
   // ランタイム設定（App Service での動的な値取得用）
   publicRuntimeConfig: {
     API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-  },
-  
-  // デバッグ用：ビルド時の環境変数確認
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      console.log('🔧 Build time NEXT_PUBLIC_API_ENDPOINT:', process.env.NEXT_PUBLIC_API_ENDPOINT);
-      console.log('🔧 All NEXT_PUBLIC_ vars:', 
-        Object.keys(process.env)
-          .filter(key => key.startsWith('NEXT_PUBLIC_'))
-          .reduce((obj: Record<string, string | undefined>, key) => {
-            obj[key] = process.env[key];
-            return obj;
-          }, {})
-      );
-    }
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
